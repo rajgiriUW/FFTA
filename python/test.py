8b481770c94fece68b82f016660e802af7b40e2a
Durmus U. Karatay <ukaratay@gmail.com>
2014-12-09 13:35:52 -0800

    Formatted logging and corrected __init__.py for utils.

--- python/test.py
+++ python/test.py
@@ -1,18 +1,27 @@
+import os
 import pixel
 import logging
 
 from utils import load
 from matplotlib import pyplot as plt
 
-logging.basicConfig(filename='C:/Users/DurmusU/SkyDrive/Research/FF-trEFM/example.log', level=logging.DEBUG, filemode='w')
+cwd = os.getcwd()
+log_file = os.path.join(cwd, 'example.log')
 
-signal_file = 'C:/Users/DurmusU/SkyDrive/Research/FF-trEFM/SW_0000.ibw'
-params_file = 'C:/Users/DurmusU/SkyDrive/Research/FF-trEFM/SW.cfg'
+logging.basicConfig(level=logging.DEBUG,
+                    format='\n%(asctime)s - %(levelname)s',
+                    datefmt='%m/%d/%y %H:%M:%S',
+                    filename=log_file,
+                    filemode='w')
+
+
+signal_file = 'C:/Users/DurmusU/SkyDrive/Projects/FFTA/data/SW_0000.ibw'
+params_file = 'C:/Users/DurmusU/SkyDrive/Projects/FFTA/data/SW.cfg'
 
 signal_array = load.signal(signal_file)
 n_pixels, params = load.configuration(params_file)
 
 p = pixel.Pixel(signal_array, params)
-p.get_tfp()
+p.analyze()
 
 plt.plot(p.inst_freq[(p.tidx - 2000):(p.tidx + 2000)])
