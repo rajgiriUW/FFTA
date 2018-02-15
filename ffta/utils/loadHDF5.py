d239d0a7340cfc8e55db0e6cee724847359fdfc5
Raj <rgiri@uw.edu>
2018-02-14 18:37:07 -0800

    Minor Fixes

--- ffta/utils/loadHDF5.py
+++ ffta/utils/loadHDF5.py
@@ -11,12 +11,48 @@ import sys
 from igor.binarywave import load as loadibw
 from numpy.lib.npyio import loadtxt
 from os.path import splitext
+import os
 
 import pycroscopy as py
 import h5py
 
-from ffta.utils.load import signal
+from ffta.utils import load
 
 
-def loadHDF5():
+def loadHDF5(path):
+    """
+    Loads .ibw folder into an HDF5 format.
+
+    Parameters
+    ----------
+    path : string
+        Path to signal file.
+
+    Returns
+    -------
+    
+
+    """
+    filelist = os.listdir(path)
+
+    data_files = [os.path.join(path, name)
+                  for name in filelist if name[-3:] == 'ibw']
+
+    config_file = [os.path.join(path, name)
+                   for name in filelist if name[-3:] == 'cfg'][0]
+    
+    #file_name = os.path.
+    
+    
     tran = px.io.NumpyTranslator()
+    
+#    h5_path = os.path.join(folder_path, file_name + '.h5')
+#
+#
+#h5_path = tran.translate(h5_path, raw_data_2d, num_rows, num_cols,
+#                         qty_name='Current', data_unit='nA', spec_name='Bias',
+#                         spec_unit='V', spec_val=volt_vec, scan_height=100,
+#                         scan_width=200, spatial_unit='nm', data_type='STS',
+#                         translator_name='ASC', parms_dict=parm_dict)
+#    
+    
