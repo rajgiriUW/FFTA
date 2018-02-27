3a1263807584c89293feafd2318455af253e590c
Raj <rgiri@uw.edu>
2018-02-26 19:56:50 -0800

    Can now process HDF file into an image via the Analyze_HDF5 function. Does not work from command line.

---
+++ ffta/analyze_HDF5.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 13:16:05 2018
+
+@author: Raj
+"""
+
+import os
+import sys
+import time
+import multiprocessing
+import logging
+import argparse as ap
+import numpy as np
+import ffta.line as line
+from ffta.utils import load
+import badpixels
+
+# Plotting imports
+import matplotlib as mpl
+#mpl.use('WxAgg')
+from matplotlib import pyplot as plt
+
+from ffta.utils import hdf_utils
+import pycroscopy as px
+from pycroscopy.io.io_utils import getTimeStamp
+
+"""
+Analyzes an HDF_5 format trEFM data set and writes the result into that file
+"""
+
+def process(h5_path):
+    """Main function of the executable file."""
+    logging.basicConfig(filename='error.log', level=logging.INFO)
+
+#    if argv is None:
+#        argv = sys.argv[1:]
+
+    # Parse arguments from the command line, and print out help.
+#    parser = ap.ArgumentParser(description='Analysis software for FF-trEFM')
+#    parser.add_argument('h5_path', nargs='?', default=os.getcwd(),
+#                        help='path to HDF5')
+#    parser.add_argument('-p', help='parallel computing option should be'
+#                        'followed by the number of CPUs.', type=int,
+#                        choices=range(2, cpu_count + 1))
+#    parser.add_argument('-v', action='version',
+#                        version='FFtr-EFM 2.0 Release Candidate')
+#    args = parser.parse_args(argv)
+
+    # Scan the path for .ibw and .cfg files.
+    #path = args.path
+
+    # Initialize file and read parameters
+#    h5_path = args.h5_path
+    hdf = px.ioHDF5(h5_path)
+    h5_main = hdf.file
+    p = px.hdf_utils.findH5group(h5_main, 'FF')[0]
+    parameters = hdf_utils.get_params(p)
+
+    n_pixels = parameters['num_cols']
+    num_rows = parameters['num_rows']
+    pnts_per_pixel = parameters['pnts_per_pixel']
+
+    print('Recombination: ', parameters['recombination'])
+    print( 'ROI: ', parameters['roi'])
+
+    # Initialize arrays.
+    tfp = np.zeros([num_rows, n_pixels])
+    shift = np.zeros([num_rows, n_pixels])
+
+    # Initialize plotting.
+    plt.ion()
+
+    fig, a = plt.subplots(nrows=2, figsize=(13, 6))
+
+#    fig = plt.figure(figsize=(12, 6), tight_layout=True)
+#    grid = gs.GridSpec(1, 2)
+    tfp_ax = a[0]
+    shift_ax = a[1]
+
+    tfp_ax.set_title('tFP Image')
+    shift_ax.set_title('Shift Image')
+
+    img_length = parameters['width']
+    img_height = parameters['height']
+
+    kwargs = {'origin': 'lower', 'aspect': 'equal', 'x_size':img_length,
+              'y_size':img_height, 'num_ticks': 5, 'stdevs': 3}
+
+    tfp_image, cbar = px.plot_utils.plot_map(tfp_ax, tfp * 1e6, cmap='inferno', **kwargs)
+    cbar.set_label('Time (us)', rotation=270, labelpad=16)
+    shift_image, cbar = px.plot_utils.plot_map(shift_ax, shift, cmap='inferno', **kwargs)
+    cbar.set_label('Frequency Shift (Hz)', rotation=270, labelpad=16)
+    text = tfp_ax.text(n_pixels/2,num_rows+3, '')
+    
+    plt.show()
+
+    # Load every file in the file list one by one.
+    for i in range(num_rows):
+
+        line_inst = hdf_utils.get_line(hdf.file, i)
+        
+        tfp[i, :], shift[i, :], _ = line_inst.analyze()
+
+        tfp_image, _ = px.plot_utils.plot_map(tfp_ax, tfp * 1e6, cmap='inferno', show_cbar=False, **kwargs)
+        shift_image, _ = px.plot_utils.plot_map(shift_ax, shift, cmap='inferno', show_cbar=False, **kwargs)
+
+        tfp_sc = tfp[tfp.nonzero()] * 1e6
+        tfp_image.set_clim(vmin=tfp_sc.min(), vmax=tfp_sc.max())
+
+        shift_sc = shift[shift.nonzero()]
+        shift_image.set_clim(vmin=shift_sc.min(), vmax=shift_sc.max())
+
+        tfpmean = 1e6 * tfp[i, :].mean()
+        tfpstd = 1e6 * tfp[i, :].std()
+
+        string = ("Line {0:.0f}, average tFP (us) ="
+                  " {1:.2f} +/- {2:.2f}".format(i + 1, tfpmean, tfpstd))
+        print(string)
+
+        text.remove()
+        text = tfp_ax.text((n_pixels-len(string))/2,num_rows+4, string)
+
+        plt.draw()
+        plt.pause(0.0001)
+
+        del line_inst  # Delete the instance to open up memory.
+
+    # Filter bad pixels
+    tfp_fixed, _ = badpixels.fix_array(tfp, threshold=2)
+    tfp_fixed = np.array(tfp_fixed)
+
+    # Save to HDF5 file. 
+    
+    # create unique suffix
+    names = hdf_utils.h5_list(hdf.file['/FFtrEFM_Group'], 'processed')
+    suffix = names[-1][-4:]
+    suffix = str(int(suffix)+1).zfill(4)
+    
+    grp_name = p.name.split('/')[-1] + '-processed-' + suffix
+    grp_tr = px.MicroDataGroup(grp_name, p.name + '/')
+
+    tfp_px = px.MicroDataset('tfp', tfp, parent=p.name)
+    shift_px = px.MicroDataset('shift', shift, parent=p.name)
+    tfp_fixed_px = px.MicroDataset('tfp_filtered', tfp_fixed, parent=p.name)
+    grp_tr.attrs['timestamp'] = getTimeStamp()
+
+    grp_tr.addChildren([tfp_px])
+    grp_tr.addChildren([shift_px])
+    grp_tr.addChildren([tfp_fixed_px])
+    
+    hdf.writeData(grp_tr, print_log=True)
+
+    hdf.flush()
+    hdf.close()
+
+    return
+
+#
+#if __name__ == '__main__':
+#
+#    sys.exit(main(sys.argv[1:]))
---
+++ ffta/utils/hdf_utils.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 14:28:28 2018
+
+@author: Raj
+"""
+import pycroscopy as px
+
+from ffta.line import Line
+from ffta.pixel import Pixel
+import numpy as np
+
+def get_params(h5_path, key='', verbose=False):
+    """
+    Gets dict of parameters from the FF-file
+    Returns a specific key-value if requested
+    
+    h5_path : str or h5py
+        Can pass either an h5_path to a file or a file already in use
+    
+    key : str, optional
+        Returns specific key in the parameters dictionary
+        
+    verbose : bool
+        Prints all parameters to console
+    """
+    
+    # path to HDF5
+    if type(h5_path)== str:
+        hdf = px.ioHDF5(h5_path)
+        p = px.hdf_utils.findH5group(hdf.file, 'FF')[0]
+    
+    # h5_path is an HDF Group
+    elif 'Group' in str(type(h5_path)):
+        p = h5_path
+    
+    # h5_path is an HDF File
+    else:
+        p = px.hdf_utils.findH5group(h5_path, 'FF')[0]
+    
+    parm_dict = {}
+    
+    for k in p.attrs:
+        parm_dict[k] = p.attrs[k]
+        
+    if verbose == True:
+        print(parm_dict)
+        
+    if any(key):
+        return parm_dict[key]
+    
+    return parm_dict
+
+
+def get_line(h5_path, line_num, array_form=False):    
+    """
+    Gets a line of data 
+    Returns a specific key if requested
+    
+    h5_path : str or h5py
+        Can pass either an h5_path to a file or a file already in use
+    
+    line_num : int
+        Returns specific line in the dataset
+        
+    array_form : bool
+        Returns the raw array contents rather than Line class
+    """
+    
+    if type(h5_path)== str:
+        hdf = px.ioHDF5(h5_path)
+        p = px.hdf_utils.findH5group(hdf.file, 'FF')[0]
+    else:
+        p = px.hdf_utils.findH5group(h5_path, 'FF')[0]
+
+    d = p['FF_raw']
+    c = p.attrs['num_cols']
+    pnts = p.attrs['pnts_per_line']
+    
+    if array_form == True:
+        return d[:, line_num*c:line_num*(c+1) , ]
+    
+    signal_array = d[:, line_num*pnts:(line_num+1)*pnts ]
+    
+    parameters = get_params(p)
+    
+    line_inst = Line(signal_array, parameters, c)
+    
+    return line_inst
+    
+
+def get_pixel(h5_path, rc, array_form=False, avg=False):    
+    """
+    Gets a pixel of data, returns all the averages within that pixel
+    Returns a specific key if requested
+    
+    h5_path : str or h5py
+        Can pass either an h5_path to a file or a file already in use
+    
+    rc : list [r, c]
+        Pixel location in terms of ROW, COLUMN
+        
+    array_form : bool
+        Returns the raw array contents rather than Pixel class
+    
+    avg : bool
+        Averages the pixels of n_pnts_per_pixel and then creates Pixel of that
+    """
+    
+    if type(h5_path)== str:
+        hdf = px.ioHDF5(h5_path)
+        p = px.hdf_utils.findH5group(hdf.file, 'FF')[0]
+    else:
+        p = px.hdf_utils.findH5group(h5_path, 'FF')[0]
+        
+    d = p['FF_raw']
+    c = p.attrs['num_cols']
+    pnts = int(p.attrs['pnts_per_pixel'])
+    
+    if array_form == True:
+        return d[:, rc[0]*c + rc[1]]
+    
+    signal_pixel = d[:, rc[0]*c + rc[1]:rc[0]*c + rc[1]+pnts]
+    parameters = get_params(p)
+    
+    if avg == True:
+        signal_pixel = signal_pixel.mean(axis=1)
+        
+    pixel_inst = Pixel(signal_pixel, parameters)
+    
+    return pixel_inst    
+
+def h5_list(h5file, key):
+    '''
+    Returns list of names matching a key in the h5 group passed
+    
+    h5file : h5py File
+        hdf.file['/Measurement_000/Channel_000'] or similar
+        
+    key : str
+        string to search for, e.g. 'processing'
+    '''
+    names = []
+    for i in h5file:
+        if key in i:
+            names.append(i)
+            
+    return names
