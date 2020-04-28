Fixes to transfer func calculation

ffta/gkpfm/transfer_func.py
@@ -14,6 +14,8 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from scipy import signal as sg
 
+import ffta
+import time
 
 def transfer_function(h5_file, tf_file = '', params_file = '', 
                       psd_freq=1e6, offset = 0.0016, sample_freq = 10e6,
@@ -140,7 +142,7 @@ def params_list(path = '', psd_freq=1e6, lift=50):
     
     return parm_dict
 
-def Y_calc(h5_main, tf=None, resampled=True, verbose=True):
+def test_Ycalc(h5_main, pixel_ind=[0,0], tf=None, resampled=True, verbose=True, noise_floor=1e-3):
     '''
     Divides the response by the transfer function
     
@@ -160,17 +162,20 @@ def Y_calc(h5_main, tf=None, resampled=True, verbose=True):
         Gives user feedback during processing
     
     '''
-    Yout = np.zeros(h5_main.shape)  # frequency domain
-    yout = np.zeros(h5_main.shape)          # time domain
+    t0 = time.time()
+
     parm_dict = usid.hdf_utils.get_attributes(h5_main)
-    ds = h5_main[()]
     
+    ds = ffta.hdf_utils.get_utils.get_pixel(h5_main, pixel_ind, array_form=True, transpose=False).flatten()
+    Yout = np.zeros(ds.shape, dtype=np.cdouble)  # frequency domain
+    Yout = np.fft.fft(ds)
+    yout = np.zeros(ds.shape)          # time domain    
     #rows = parm_dict['num_rows']
     #cols = parm_dict['num_cols']
     
     # Create frequency axis for the pixel
     samp = parm_dict['sampling_rate']
-    fq_y = np.linspace(0, samp,  Yout.shape[1]) 
+    fq_y = np.linspace(0, samp,  len(Yout)) 
     
     # Get the transfer function and transfer function frequency values
     fq_tf = h5_main.file['Transfer_Function/Freq']
@@ -191,28 +196,113 @@ def Y_calc(h5_main, tf=None, resampled=True, verbose=True):
     #   c) move to next frequency
     # FFT division is Yout
     # iFFT of the results, that is yout
+
+    t1 = time.time()
+    print('Time for pixels:', t1 - t0)
+    Yout_divided = np.zeros(len(yout), dtype=bool)
+
+    TFratios = np.ones(len(tf))
+
+    for x, f in zip(tf, fq_tf):
+        
+        if np.abs(x) > noise_floor:
+            
+            xx = np.searchsorted(fq_y, f)
+            if not Yout_divided[xx]: 
+                TFratios[xx] = x
+                Yout[xx] /= x
+                Yout[-xx] /= x
+                Yout_divided[xx] = True
+    
+    yout= np.real(np.fft.ifft(Yout))
+    
+    t2 = time.time()
+    
+    print('Time for pixels:', t2 - t1)
+    return  TFratios, Yout, yout
+
+def Y_calc(h5_main, tf=None, resampled=True, ratios=None, verbose=True, noise_floor=1e-3):
+    '''
+    Divides the response by the transfer function
+    
+    This processes every pixel, so it takes awhile
+    
+    h5_main : h5py dataset of USIDataset
+    
+    tf : transfer function, optional
+        This can be the resampled or normal transfer function
+        For best results, use the "normalized" transfer function
+        "None" will default to /Transfer_Function folder
+        
+    resampled : bool, optional
+        Whether to use the upsampled Transfer Function or the original
+        
+    verbose: bool, optional
+        Gives user feedback during processing
+    
+    ratios : array, optional
+        Array of the size of h5_main (1-D) with the transfer function data
+        If not given, it's found via the test_Y_calc function
+    
+    '''
+    Yout = np.zeros(h5_main.shape, dtype=np.cdouble)  # frequency domain
+    yout = np.zeros(h5_main.shape)          # time domain
+    parm_dict = usid.hdf_utils.get_attributes(h5_main)
+    ds = h5_main[()]
+    
+    #rows = parm_dict['num_rows']
+    #cols = parm_dict['num_cols']
+    
+    # Create frequency axis for the pixel
+    samp = parm_dict['sampling_rate']
+    fq_y = np.linspace(0, samp,  Yout.shape[1]) 
+    
+    # Get the transfer function and transfer function frequency values
+    fq_tf = h5_main.file['Transfer_Function/Freq']
+    if not tf:
+        if resampled:
+    
+            tf = h5_main.file['Transfer_Function/TFnorm_resampled'][()]
+            fq_tf = h5_main.file['Transfer_Function/Freq_resampled'][()]
+            ratios, _, _ = test_Ycalc(h5_main, resampled=True)
+        else:
+            
+            tf = h5_main.file['Transfer_Function/TFnorm'][()]
+            ratios, _, _ = test_Ycalc(h5_main, resampled=False)
+    # FFT of that pixel
+    # For each frequency in the FFT
+    #   a) Find frequency in the transfer function
+    #   b) Divide the results at that frequency
+    #   c) move to next frequency
+    # FFT division is Yout
+    # iFFT of the results, that is yout
     import time
     t0 = time.time()
 
     for c in np.arange(h5_main.shape[0]):
-        
+    
         if verbose:
             if c%100 == 0:
                 print('Pixel:', c)
+                
+        Yout[c, :] = np.fft.fft(ds[c,:])
+        Yout[c, :] /= ratios[c]
+
+        # Yout[c,:] = np.fft.fft(ds[c,:])
+        # Yout_divided = np.zeros(len(ds), dtype=bool)
         
-        Ypxl = np.fft.fft(ds[c,:])
-        Yout_divided = np.zeros(len(ds), dtype=bool)
-        
-        for x, f in zip(tf, fq_tf):
-        
-            xx = np.searchsorted(fq_tf, f)
+        # for x, f in zip(tf, fq_tf):
+            
+        #     if np.abs(x) > noise_floor:
+                
+        #         xx = np.searchsorted(fq_y, f)
            
-            if not Yout_divided[xx]: 
-                Yout[c,x] = Ypxl[x] / tf[xx] 
-                Yout[c,-x] = Ypxl[x] / tf[xx] 
-                Yout_divided[xx] = True
+        #         if not Yout_divided[xx]: 
+        #             Yout[c,xx] /= x
+        #             Yout[c,-xx] /= x
+        #             Yout_divided[xx] = True
         
-        yout[c,:] = np.fft.ifft(Yout[c,:])
+        yout[c,:] = np.real(np.fft.ifft(Yout[c,:]))
     
     t1 = time.time()
     
@@ -282,7 +372,7 @@ def save_Yout(h5_main, Yout, yout):
                                              'V',  # Units for the physical quantity
                                              pos_desc,  # Position dimensions
                                              spec_desc,  # Spectroscopic dimensions
-                                             dtype=np.float32,  # data type / precision
+                                             dtype=np.cdouble,  # data type / precision
                                              main_dset_attrs=parm_dict)
 
     usid.hdf_utils.copy_attributes(h5_y, h5_gp)

ffta/hdf_utils/filtering.py
@@ -18,6 +18,136 @@ from matplotlib import pyplot as plt
 
 import warnings
 
+def test_filter(hdf_file, freq_filts, parameters={}, pixelnum=[0, 0], noise_tolerance=5e-7,
+                show_plots=True, check_filter=True):
+    """
+    Applies FFT Filter to the file at a specific line and displays the result
+
+    hdf_file : h5Py file or Nx1 NumPy array (preferred is NumPy array)
+        hdf_file to work on, e.g. hdf.file['/FF-raw'] if that's a Dataset
+        if ndarray, uses passed or default parameters
+        Use ndarray.flatten() to ensure correct dimensions
+
+    freq_filts : list of FrequencyFilter class objects
+        Contains the filters to apply to the test signal
+
+    parameters : dict, optional
+        Contains parameters in FF-raw file for constructing filters. Automatic if a Dataset/File
+        Must contain num_pts and samp_rate to be functional
+    
+    pixelnum : int, optional
+        For extracting a specific pixel to do FFT Filtering on
+        
+    show_plots : bool, optional
+        Turns on FFT plots from Pycroscopy
+
+    noise_tolerance : float 0 to 1
+        Amount of noise below which signal is set to 0
+
+    Returns
+    -------
+    filt_line : numpy.ndarray
+        Filtered signal of hdf_file
+    
+    freq_filts : list
+        The filter parameters to be passed to SignalFilter
+    
+    fig_filt, axes_filt: matplotlib controls
+        Only functional if show_plots is on
+    """
+
+    reshape = False
+    ftype = str(type(hdf_file))
+    if ('h5py' in ftype) or ('Dataset' in ftype):  # hdf file
+
+        parameters = get_utils.get_params(hdf_file)
+        hdf_file = get_utils.get_pixel(hdf_file, [pixelnum[0], pixelnum[1]], array_form=True, transpose=False)
+        hdf_file = hdf_file.flatten()
+
+    if len(hdf_file.shape) == 2:
+        reshape = True
+        hdf_file = hdf_file.flatten()
+
+    sh = hdf_file.shape
+
+    # Test filter on a single line:
+    filt_line, fig_filt, axes_filt = px.processing.gmode_utils.test_filter(hdf_file,
+                                                                           frequency_filters=freq_filts,
+                                                                           noise_threshold=noise_tolerance,
+                                                                           show_plots=show_plots)
+
+    # If need to reshape
+    if reshape:
+        filt_line = np.reshape(filt_line, sh)
+
+    # Test filter out in Pixel
+    if check_filter:
+        plt.figure()
+        plt.plot(hdf_file, 'b')
+        plt.plot(filt_line, 'k')
+
+        h5_px_filt = pixel.Pixel(filt_line, parameters)
+        h5_px_filt.clear_filter_flags()
+        h5_px_filt.analyze()
+        h5_px_filt.plot(newplot=True)
+
+        h5_px_raw = pixel.Pixel(hdf_file, parameters)
+        h5_px_raw.analyze()
+        h5_px_raw.plot(newplot=True)
+
+    #    h5_px_raw_unfilt = pixel.Pixel(hdf_file, parameters)
+    #    h5_px_raw_unfilt.clear_filter_flags()
+    #    h5_px_raw_unfilt.analyze()
+    #    h5_px_raw_unfilt.plot(newplot=False,c1='y', c2='c')
+
+    return filt_line, freq_filts, fig_filt, axes_filt
+
+
+def fft_filter(h5_main, freq_filts, noise_tolerance=5e-7, make_new=False, verbose=False):
+    """
+    Stub for applying filter above to the entire FF image set
+    
+    h5_main : h5py.Dataset object
+        Dataset to work on, e.g. h5_main = px.hdf_utils.getDataSet(hdf.file, 'FF_raw')[0]
+    
+    freq_filts : list
+        List of frequency filters usually generated in test_line above
+        
+    noise_tolerance : float, optional
+        Level below which data are set to 0. Higher values = more noise (more tolerant)
+    
+    make_new : bool, optional
+        Allows for re-filtering the data by creating a new folder
+    
+    Returns
+    -------
+    
+    h5_filt : Dataset
+        Filtered dataset within latest -FFT_Filtering Group
+        
+    """
+
+    h5_filt_grp = usid.hdf_utils.check_for_old(h5_main, 'FFT_Filtering')
+
+    if make_new == True or not any(h5_filt_grp):
+
+        sig_filt = px.processing.SignalFilter(h5_main, frequency_filters=freq_filts,
+                                              noise_threshold=noise_tolerance,
+                                              write_filtered=True, write_condensed=False,
+                                              num_pix=1, verbose=verbose, cores=2, max_mem_mb=512)
+
+        h5_filt_grp = sig_filt.compute()
+
+    else:
+        print('Taking previously computed results')
+        h5_filt = h5_filt_grp[0]['Filtered_Data']
+
+    h5_filt = h5_filt_grp['Filtered_Data']
+    usid.hdf_utils.copy_attributes(h5_main.parent, h5_filt)
+    usid.hdf_utils.copy_attributes(h5_main.parent, h5_filt.parent)
+
+    return h5_filt
+
 
 def lowpass(hdf_file, parameters={}, pixelnum=[0, 0], f_cutoff=None):
     '''
@@ -144,137 +274,6 @@ def _get_pixel_for_filtering(hdf_file, parameters={}, pixelnum=[0, 0]):
     samp_rate = parameters['sampling_rate']
 
     return hdf_file, num_pts, drive, samp_rate
-
-
-def test_filter(hdf_file, freq_filts, parameters={}, pixelnum=[0, 0], noise_tolerance=5e-7,
-                show_plots=True, check_filter=True):
-    """
-    Applies FFT Filter to the file at a specific line and displays the result
-
-    hdf_file : h5Py file or Nx1 NumPy array (preferred is NumPy array)
-        hdf_file to work on, e.g. hdf.file['/FF-raw'] if that's a Dataset
-        if ndarray, uses passed or default parameters
-        Use ndarray.flatten() to ensure correct dimensions
-
-    freq_filts : list of FrequencyFilter class objects
-        Contains the filters to apply to the test signal
-
-    parameters : dict, optional
-        Contains parameters in FF-raw file for constructing filters. Automatic if a Dataset/File
-        Must contain num_pts and samp_rate to be functional
-    
-    pixelnum : int, optional
-        For extracting a specific pixel to do FFT Filtering on
-        
-    show_plots : bool, optional
-        Turns on FFT plots from Pycroscopy
-
-    noise_tolerance : float 0 to 1
-        Amount of noise below which signal is set to 0
-
-    Returns
-    -------
-    filt_line : numpy.ndarray
-        Filtered signal of hdf_file
-    
-    freq_filts : list
-        The filter parameters to be passed to SignalFilter
-    
-    fig_filt, axes_filt: matplotlib controls
-        Only functional if show_plots is on
-    """
-
-    reshape = False
-    ftype = str(type(hdf_file))
-    if ('h5py' in ftype) or ('Dataset' in ftype):  # hdf file
-
-        parameters = get_utils.get_params(hdf_file)
-        hdf_file = get_utils.get_pixel(hdf_file, [pixelnum[0], pixelnum[1]], array_form=True, transpose=False)
-        hdf_file = hdf_file.flatten()
-
-    if len(hdf_file.shape) == 2:
-        reshape = True
-        hdf_file = hdf_file.flatten()
-
-    sh = hdf_file.shape
-
-    # Test filter on a single line:
-    filt_line, fig_filt, axes_filt = px.processing.gmode_utils.test_filter(hdf_file,
-                                                                           frequency_filters=freq_filts,
-                                                                           noise_threshold=noise_tolerance,
-                                                                           show_plots=show_plots)
-
-    # If need to reshape
-    if reshape:
-        filt_line = np.reshape(filt_line, sh)
-
-    # Test filter out in Pixel
-    if check_filter:
-        plt.figure()
-        plt.plot(hdf_file, 'b')
-        plt.plot(filt_line, 'k')
-
-        h5_px_filt = pixel.Pixel(filt_line, parameters)
-        h5_px_filt.clear_filter_flags()
-        h5_px_filt.analyze()
-        h5_px_filt.plot(newplot=True, c1='b', c2='k')
-
-        h5_px_raw = pixel.Pixel(hdf_file, parameters)
-        h5_px_raw.analyze()
-        h5_px_raw.plot(newplot=True, c1='b', c2='k')
-
-    #    h5_px_raw_unfilt = pixel.Pixel(hdf_file, parameters)
-    #    h5_px_raw_unfilt.clear_filter_flags()
-    #    h5_px_raw_unfilt.analyze()
-    #    h5_px_raw_unfilt.plot(newplot=False,c1='y', c2='c')
-
-    return filt_line, freq_filts, fig_filt, axes_filt
-
-
-def fft_filter(h5_main, freq_filts, noise_tolerance=5e-7, make_new=False, verbose=False):
-    """
-    Stub for applying filter above to the entire FF image set
-    
-    h5_main : h5py.Dataset object
-        Dataset to work on, e.g. h5_main = px.hdf_utils.getDataSet(hdf.file, 'FF_raw')[0]
-    
-    freq_filts : list
-        List of frequency filters usually generated in test_line above
-        
-    noise_tolerance : float, optional
-        Level below which data are set to 0. Higher values = more noise (more tolerant)
-    
-    make_new : bool, optional
-        Allows for re-filtering the data by creating a new folder
-    
-    Returns
-    -------
-    
-    h5_filt : Dataset
-        Filtered dataset within latest -FFT_Filtering Group
-        
-    """
-
-    h5_filt_grp = usid.hdf_utils.check_for_old(h5_main, 'FFT_Filtering')
-
-    if make_new == True or not any(h5_filt_grp):
-
-        sig_filt = px.processing.SignalFilter(h5_main, frequency_filters=freq_filts,
-                                              noise_threshold=noise_tolerance,
-                                              write_filtered=True, write_condensed=False,
-                                              num_pix=1, verbose=verbose, cores=2, max_mem_mb=512)
-
-        h5_filt_grp = sig_filt.compute()
-
-    else:
-        print('Taking previously computed results')
-        h5_filt = h5_filt_grp[0]['Filtered_Data']
-
-    h5_filt = h5_filt_grp['Filtered_Data']
-    usid.hdf_utils.copy_attributes(h5_main.parent, h5_filt)
-    usid.hdf_utils.copy_attributes(h5_main.parent, h5_filt.parent)
-
-    return h5_filt
 
 
 # placeholder until accepted in pull request