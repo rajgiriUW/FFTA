d3364a25cb4954cfc8ea32e20e569184ca1e3f36
Raj <rgiri@uw.edu>
2020-08-01 21:22:44 -0700

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='FFTA',
-    version='0.2.1',
+    version='0.3.0',
     description='Fast Free Transient Analysis',
     long_description=long_description,
     long_description_content_type='text/markdown',
