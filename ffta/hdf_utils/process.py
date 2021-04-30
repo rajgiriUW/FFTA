daea9a6da081901f447f6fce54e74ac824eae3e2
Raj <rgiri@uw.edu>
2021-04-29 23:15:24 -0700

    calibration fix in Process

--- ffta/hdf_utils/process.py
+++ ffta/hdf_utils/process.py
@@ -311,7 +311,7 @@ class FFtrEFM(Process):
             
             if 'tfp_cal' in self.h5_results_grp:
                 del self.h5_results_grp['tfp_cal']
-            tfp_cal = cal(h5_tfp)
+            tfp_cal = np.exp(cal(np.log(h5_tfp))) # cal spline is by default in log space
             self.h5_tfp_cal = self.h5_results_grp.create_dataset('tfp_cal', 
                                                                  data=tfp_cal, 
                                                                  dtype=np.float32)
@@ -319,7 +319,7 @@ class FFtrEFM(Process):
             if 'cal_curve' in self.h5_results_grp:
                 del self.h5_results_grp['cal_curve']
             _ = self.h5_results_grp.create_dataset('cal_curve',
-                                                   data = np.array([tfps, cal(tfps)]),
+                                                   data = np.array([tfps, cal(np.log(tfps))]),
                                                    dtype=np.float32)
 
         return
